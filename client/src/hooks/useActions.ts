c07f4b10ba6c60092e81e9f487e3c001414d7f96
Zhedsnake <besedinzena5@gmail.com>
2024-08-12 12:32:31 +0300

    Документация хуков

--- client/src/hooks/useActions.ts
+++ client/src/hooks/useActions.ts
@@ -2,6 +2,23 @@ import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import ActionCreators from '../store/action-creators/'
 
+/**
+ * Хук, который возвращает объект с привязанными действиями (action creators) редукторов redux для использования в компонентах.
+ *
+ * Этот хук упрощает вызов действий в компонентах, так как не нужно вручную вызывать `dispatch` для каждого действия.
+ * Ипользует `useDispatch` для получения функции `dispatch` из Redux store и привязывает все действия из
+ * `ActionCreators` к этой функции с помощью `bindActionCreators`.
+ *
+ * @returns {Object} Объект с действиями, привязанными к функции `dispatch`. Каждое действие из `ActionCreators`
+ * будет доступно как метод объекта.
+ *
+ * @example
+ * const { getWordsAction, defWordsAction } = useActions();
+ *
+ * // Теперь можно вызывать действия без использования dispatch напрямую
+ * getWordsAction();
+ * defWordsAction();
+ */
 export const useActions = () => {
     const dispatch = useDispatch()
     return bindActionCreators(ActionCreators, dispatch)
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -3,12 +3,30 @@ import Home from "../pages/home.tsx";
 
 
 
+/**
+ * Интерфейс, описывающий маршрут приложения.
+ *
+ * @interface RoutesTypes
+ * @property {string} path - Путь маршрута, который будет использоваться в приложении.
+ * @property {React.ReactNode} component - Компонент, который будет рендериться при переходе на данный маршрут.
+ */
 export interface RoutesTypes {
     path: string,
     component: React.ReactNode
 }
 
 
+/**
+ * Массив маршрутов для публичной части приложения.
+ *
+ * @constant {RoutesTypes[]} publicRoutes
+ * @description
+ * Этот массив содержит список маршрутов, доступных для всех пользователей приложения.
+ * Каждый маршрут представляет собой объект с путем и компонентом, который будет отображаться при переходе на этот путь.
+ *
+ * Пример:
+ * - Путь `'/'` будет отображать компонент `<Home />`.
+ */
 const publicRoutes: RoutesTypes[] = [
     {path: '/', component: <Home />},
 ]
